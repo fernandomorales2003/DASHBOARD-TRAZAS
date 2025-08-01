593afd93011b1d254d4c5626fc4e6de642512993
Fernando <fernando_morales2003@hotmail.com>
2025-08-01 16:54:01 -0300

    Update streamlt_app_dashboard.py

--- streamlt_app_dashboard.py
+++ streamlt_app_dashboard.py
@@ -79,7 +79,6 @@ with col1:
 
 with col2:
     st.subheader("📌 Estado de Enlaces (KPI)")
-
     enlaces_info = {
         "MZA-FTTH-01": {"Tx": 0, "Rx_cert": -20},
         "MZA-FTTH-02": {"Tx": 5, "Rx_cert": -20},
@@ -130,20 +129,20 @@ with col2:
     df = pd.DataFrame(datos)
 
     for row in df.itertuples():
-    icono, color = estado_icono_color(row.Estado)
-    st.markdown(f"""
-        <div style="background-color:{color};
-                    padding:8px 20px;
-                    border-radius:10px;
-                    text-align:center;
-                    color:white;
-                    margin-bottom:12px;
-                    width:100%;
-                    height:auto;">
-            <h5 style="margin: 4px 0;">{row.Enlace}</h5>
-            <p style="font-size:18px; margin: 4px 0;">{icono} <strong>{row.Estado}</strong></p>
-        </div>
-    """, unsafe_allow_html=True)
+        icono, color = estado_icono_color(row.Estado)
+        st.markdown(f"""
+            <div style="background-color:{color};
+                        padding:8px 20px;
+                        border-radius:10px;
+                        text-align:center;
+                        color:white;
+                        margin-bottom:12px;
+                        width:100%;
+                        height:auto;">
+                <h5 style="margin: 4px 0;">{row.Enlace}</h5>
+                <p style="font-size:18px; margin: 4px 0;">{icono} <strong>{row.Estado}</strong></p>
+            </div>
+        """, unsafe_allow_html=True)
 
 
 with col3:
