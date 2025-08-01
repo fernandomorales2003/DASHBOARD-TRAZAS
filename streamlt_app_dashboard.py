1e3ed8195319cfd3fa0a46b1da45a1e3cc628a6f
Fernando <fernando_morales2003@hotmail.com>
2025-08-01 16:57:03 -0300

    Update streamlt_app_dashboard.py

--- streamlt_app_dashboard.py
+++ streamlt_app_dashboard.py
@@ -140,7 +140,7 @@ with col2:
                         width:100%;
                         height:auto;">
                 <h5 style="margin: 4px 0;">{row.Enlace}</h5>
-                <p style="font-size:10px; margin: 4px 0;">{icono} <strong>{row.Estado}</strong></p>
+                <p style="font-size:14px; margin: 4px 0;">{icono} <strong>{row.Estado}</strong></p>
             </div>
         """, unsafe_allow_html=True)
 
