a4b2388e2e1fe898454ccde1b8dec3d7331bdb3b
Fernando <fernando_morales2003@hotmail.com>
2025-08-01 15:48:19 -0300

    Update streamlt_app_dashboard.py

--- streamlt_app_dashboard.py
+++ streamlt_app_dashboard.py
@@ -214,23 +214,19 @@ st.pydeck_chart(pdk.Deck(
     initial_view_state=view_state,
     tooltip=tooltip
 ))
+
+if corte_activo and distancia_corte > 0:
+        corte_detectado = True
+    
 # --- Indicador para TR1-SUR
 if traza_seleccionada == "TR1-SUR":
+    # Detectar corte si el usuario lo indicó desde el sidebar
+    if corte_activo and distancia_corte > 0:
+        corte_detectado = True
+
     st.markdown("### Distribución de Clientes")
     col1, col2 = st.columns(2)
-
-    # Si corte_detectado no existe, asumimos False
-    try:
-        if corte_detectado:
-            col1.metric("Clientes operativos", "0")
-        else:
-            col1.metric("Clientes operativos", "750")
-    except NameError:
-        col1.metric("Clientes operativos", "750")
-
-    col2.metric("Cliente", "WISP")
-
-
+    
 # --- Radar Chart y Barras Apiladas en Tabs
 if traza_seleccionada == "TR-S-DER-02":
     clientes_por_hub = trazas["TR-S-DER-02"]["clientes_hubs"]
